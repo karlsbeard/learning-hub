c308780316769c68c7eba48c0678234df083381a
karlsbeard <50100424+karlsbeard@users.noreply.github.com>
2024-10-31 14:07:41 +0000

    Deploying to build from @ karlsbeard/learning-hub@434c1422c26ecc9a56d029608fb31ecca2338c33 🚀

---
+++ meta__docs.3999ee81.async.js
@@ -0,0 +1,77 @@
+"use strict";(self.webpackChunklearning_hub=self.webpackChunklearning_hub||[]).push([[904],{43841:function(t,n,e){e.r(n),e.d(n,{demos:function(){return r}});var d=e(67294),a=e(61073),r={}},21682:function(t,n,e){e.r(n),e.d(n,{demos:function(){return r}});var d=e(67294),a=e(11024),r={}},60810:function(t,n,e){e.r(n),e.d(n,{demos:function(){return r}});var d=e(67294),a=e(25223),r={}},11171:function(t,n,e){e.r(n),e.d(n,{demos:function(){return r}});var d=e(67294),a=e(68537),r={}},58500:function(t,n,e){e.r(n),e.d(n,{texts:function(){return a}});var d=e(61073);const a=[{value:`typeof \u68C0\u6D4B null \u8FD4\u56DE\u662F object
+typeof \u4E00\u4E2A\u6CA1\u6709\u503C\u7684\u53D8\u91CF\u4F1A\u8FD4\u56DE undefined`,paraId:0,tocIndex:1},{value:"null\u662F\u4E00\u4E2A\u53EA\u6709\u4E00\u4E2A\u503C\u7684\u7279\u6B8A\u7C7B\u578B\u3002\u8868\u793A\u4E00\u4E2A\u7A7A\u5BF9\u8C61\u5F15\u7528\u3002",paraId:1,tocIndex:2},{value:"undefined \u662F\u4E00\u4E2A\u6CA1\u6709\u8BBE\u7F6E\u503C\u7684\u53D8\u91CF\u3002",paraId:1,tocIndex:2},{value:"never \u662F\u5176\u5B83\u7C7B\u578B\uFF08\u5305\u62EC null \u548C undefined\uFF09\u7684\u5B50\u7C7B\u578B\uFF0C\u4EE3\u8868\u4ECE\u4E0D\u4F1A\u51FA\u73B0\u7684\u503C\u3002\u58F0\u660E\u4E3A never \u7C7B\u578B\u7684\u53D8\u91CF\u53EA\u80FD\u88AB never \u7C7B\u578B\u6240\u8D4B\u503C",paraId:1,tocIndex:2},{value:"?? \u548C || \u7684\u533A\u522B",paraId:2,tocIndex:2},{value:`\u76F8\u540C\u70B9
+`,paraId:3,tocIndex:2},{value:"\u7528\u6CD5\u76F8\u540C\uFF0C\u524D\u540E\u662F\u503C\uFF0C\u4E2D\u95F4\u7528\u7B26\u53F7\u8FDE\u63A5",paraId:4,tocIndex:2},{value:"\u503C1 ?? \u503C2",paraId:4,tocIndex:2},{value:"\u503C1 || \u503C2",paraId:4,tocIndex:2},{value:`\u4E0D\u540C\u70B9
+`,paraId:3,tocIndex:2},{value:`\u5224\u65AD\u65B9\u5F0F\u4E0D\u540C\uFF1A
+`,paraId:5,tocIndex:2},{value:"\u4F7F\u7528",paraId:6,tocIndex:2},{value:"??",paraId:6,tocIndex:2},{value:"\u65F6\uFF0C\u53EA\u6709\u5F53",paraId:6,tocIndex:2},{value:"\u503C1",paraId:6,tocIndex:2},{value:"\u4E3A",paraId:6,tocIndex:2},{value:"null",paraId:6,tocIndex:2},{value:"\u6216",paraId:6,tocIndex:2},{value:"undefined",paraId:6,tocIndex:2},{value:"\u65F6\u624D\u8FD4\u56DE",paraId:6,tocIndex:2},{value:"\u503C2",paraId:6,tocIndex:2},{value:"\u4F7F\u7528",paraId:6,tocIndex:2},{value:"||",paraId:6,tocIndex:2},{value:"\u65F6\uFF0C",paraId:6,tocIndex:2},{value:"\u503C1",paraId:6,tocIndex:2},{value:"\u4F1A\u8F6C\u6362\u4E3A\u5E03\u5C14\u503C\u5224\u65AD\uFF0C\u4E3A",paraId:6,tocIndex:2},{value:"true",paraId:6,tocIndex:2},{value:"\u65F6\u8FD4\u56DE",paraId:6,tocIndex:2},{value:"\u503C1",paraId:6,tocIndex:2},{value:",",paraId:6,tocIndex:2},{value:"false",paraId:6,tocIndex:2},{value:"\u8FD4\u56DE",paraId:6,tocIndex:2},{value:"\u503C2",paraId:6,tocIndex:2},{value:"type \u548C interface\u7684\u533A\u522B",paraId:7,tocIndex:2},{value:"\u76F8\u540C\u70B9",paraId:8,tocIndex:2},{value:"\u90FD\u53EF\u63CF\u8FF0\u4E00\u4E2A\u5BF9\u8C61\u6216\u51FD\u6570",paraId:9,tocIndex:2},{value:"interface",paraId:10,tocIndex:2},{value:`  interface User {
+    name: string
+    age: number
+  }
+  
+  interface SetUser {
+    (name: string, age: number): void;
+  }
+`,paraId:11,tocIndex:2},{value:"type",paraId:12,tocIndex:2},{value:`  type User {
+    name: string
+    age: number
+  }
+  type SetUser = (name: string, age: number)=> void
+`,paraId:13,tocIndex:2},{value:"\u90FD\u5141\u8BB8\u62D3\u5C55",paraId:14,tocIndex:2},{value:"interface\u548Ctype\u90FD\u53EF\u4EE5\u62D3\u5C55\uFF0C\u5E76\u4E14\u4E24\u8005\u5E76\u4E0D\u662F\u76F8\u4E92\u72EC\u7ACB\u7684\uFF0C\u4E5F\u5C31\u662F\u8BF4interface\u53EF\u4EE5extends type,type\u4E5F\u53EF\u4EE5 extends interface\u3002\u867D\u7136\u6548\u679C\u5DEE\u4E0D\u591A\uFF0C\u4F46\u662F\u4E24\u8005\u8BED\u6CD5\u4E0D\u540C\u3002",paraId:15,tocIndex:2},{value:"interface extends interface",paraId:16,tocIndex:2},{value:`  interface Name {
+    name: string
+  }
+  interface User extend Name  {
+    age: number
+  }
+`,paraId:17,tocIndex:2},{value:"type extends type",paraId:18,tocIndex:2},{value:`  type Name = {
+    name: string
+  }
+  type User = Name & {age: number}
+`,paraId:19,tocIndex:2},{value:"interface extend type",paraId:20,tocIndex:2},{value:`  type Name = {
+    name: string
+  }
+  interface User extends Name {
+    age: number
+  }
+`,paraId:21,tocIndex:2},{value:"type extends interface",paraId:22,tocIndex:2},{value:`  interface Name {
+    name: string
+  }
+  type User = Name &  {
+    age: number
+  }
+`,paraId:23,tocIndex:2},{value:"\u4E0D\u540C\u70B9",paraId:24,tocIndex:2},{value:"type\u53EF\u4EE5\u4F46interface\u4E0D\u53EF",paraId:25,tocIndex:2},{value:"type\u53EF\u4EE5\u58F0\u660E\u57FA\u672C\u7C7B\u578B\u522B\u540D\uFF0C\u8054\u5408\u7C7B\u578B\uFF0C\u5143\u7EC4\u7B49\u7C7B\u578B",paraId:26,tocIndex:2},{value:`  type Name = string
+
+  interface Dog {
+    wong()
+  }
+  interface Cat {
+    miao()
+  }
+
+  type Pet = Dog | Cat
+
+  type PetList = [Dog, Pet]
+`,paraId:27,tocIndex:2},{value:"type\u8BED\u53E5\u8FD8\u53EF\u4F7F\u7528typeof\u83B7\u53D6\u5B9E\u4F8B\u7684 \u7C7B\u578B\u8FDB\u884C\u8D4B\u503C",paraId:28,tocIndex:2},{value:`  let div = document.createElement('div')
+  type B = typeof div
+`,paraId:29,tocIndex:2},{value:"\u9A9A\u64CD\u4F5C",paraId:30,tocIndex:2},{value:`  type StringOrNumber = string | number;
+  type Text = string | {text: string};
+  type NameLookup = Dictionary<string, Person>;
+  type Callback<T> = (data: T) => void;
+  type Pair<T> = [T, T];
+  type Coordinates = Pair<number>;
+  type Tree<T> = T | { left: Tree<T>, right: Tree<T>};        
+`,paraId:31,tocIndex:2},{value:"interface \u53EF\u4EE5\u4F46\u662Ftype\u4E0D\u53EF\u4EE5",paraId:32,tocIndex:2},{value:"interface\u80FD\u591F\u58F0\u660E\u5408\u5E76",paraId:33,tocIndex:2},{value:`  interface User {
+    name: string
+    age: number
+  }
+
+  interface User {
+    sex: string
+  }
+
+  /*
+  User \u63A5\u53E3\u4E3A {
+    name: string
+    age: number
+    sex: string 
+  }
+  */
+`,paraId:34,tocIndex:2}]},56650:function(t,n,e){e.r(n),e.d(n,{texts:function(){return a}});var d=e(11024);const a=[]},87799:function(t,n,e){e.r(n),e.d(n,{texts:function(){return a}});var d=e(25223);const a=[{value:"This is a guide example.",paraId:0}]},51446:function(t,n,e){e.r(n),e.d(n,{texts:function(){return a}});var d=e(68537);const a=[{value:"learning-hub",paraId:0}]}}]);
