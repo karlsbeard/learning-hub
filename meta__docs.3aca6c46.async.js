d99956022f92f7a3ef2ccd729584454281561f46
chengkai48 <chengkai48@jd.com>
2024-12-05 18:42:16 +0800

    Updates

---
+++ meta__docs.3aca6c46.async.js
@@ -0,0 +1,142 @@
+"use strict";(self.webpackChunklearning_hub=self.webpackChunklearning_hub||[]).push([[904],{59337:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(26236),o={}},75835:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(61298),o={}},73460:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(21611),o={}},17088:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(71348),o={}},38354:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(24044),o={}},79006:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(87942),o={}},84052:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(78999),o={}},70379:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(87725),o={}},77985:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(73723),o={}},78474:function(t,a,e){e.r(a),e.d(a,{demos:function(){return o}});var d=e(75271),n=e(89489),o={}},55198:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(26236);const n=[{value:"\u5728\u5FAE\u524D\u7AEF\u9879\u76EE\u4E2D\uFF0C\u9700\u8981\u5C06\u5B50\u5E94\u7528\u914D\u7F6E\u8FDB\u5165\u5BF9\u5E94\u7684\u57FA\u5EA7\u5E94\u7528\u4E2D\uFF0C\u7136\u800C\u5728\u5D4C\u5165\u8FC7\u7A0B\u4E2D\u9047\u5230\u4E86\u4E00\u4E9B\u62A5\u9519\u4FE1\u606F",paraId:0,tocIndex:2},{value:"\u57FA\u5EA7\u5E94\u7528: S",paraId:1},{value:"\u5B50\u5E94\u7528: react + vite",paraId:1},{value:"\u5FAE\u524D\u7AEF\u6846\u67B6: micro-app",paraId:1},{value:`Access to fetch at 'http:xxx' from origin 'http://xxxb' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+`,paraId:2,tocIndex:3},{value:"\u8BF7\u6C42\u4E2D\u7684 credentials \u6A21\u5F0F\u88AB\u8BBE\u7F6E\u4E3A include\uFF0C\u8FD9\u901A\u5E38\u662F\u4E3A\u4E86\u652F\u6301\u5E26\u6709\u7528\u6237\u8BA4\u8BC1\u7684\u8BF7\u6C42\uFF08\u5982 Cookie\uFF09\u3002",paraId:3,tocIndex:4},{value:"\u5F53\u4F7F\u7528 credentials: 'include' \u8FDB\u884C\u8DE8\u57DF\u8BF7\u6C42\u65F6\uFF0C\u540E\u7AEF\u7684 Access-Control-Allow-Origin \u5934\u4E0D\u80FD\u8BBE\u7F6E\u4E3A\u901A\u914D\u7B26 *\uFF0C\u800C\u5FC5\u987B\u6307\u5B9A\u660E\u786E\u7684\u57DF\u3002",paraId:3,tocIndex:4}]},29798:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(61298);const n=[]},22885:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(21611);const n=[]},98467:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(71348);const n=[{value:"\u4E00\u4E2A\u5341\u5206\u4F18\u79C0\u7684\u5DE5\u5177\uFF0C\u53EF\u4EE5\u5F88\u5927\u7A0B\u5EA6\u4E0A\u4E3A\u7A0B\u5E8F\u5458\u8FDB\u884C\u63D0\u6548",paraId:0,tocIndex:0},{value:"\u5B98\u65B9\u5730\u5740: ",paraId:0,tocIndex:0},{value:"https://github.com/ohmyzsh/ohmyzsh",paraId:0,tocIndex:0},{value:"curl",paraId:1,tocIndex:3},{value:`  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+`,paraId:2,tocIndex:3},{value:"wget",paraId:3,tocIndex:3},{value:`  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+`,paraId:4,tocIndex:3},{value:"fetch",paraId:5,tocIndex:3},{value:`  sh -c "$(fetch -o - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+`,paraId:6,tocIndex:3},{value:"zsh-autosuggestions",paraId:7,tocIndex:4},{value:"\u4F5C\u7528\uFF1A\u81EA\u52A8\u8865\u5168\u547D\u4EE4\uFF0C\u5988\u5988\u518D\u4E5F\u4E0D\u7528\u62C5\u5FC3\u6211\u8BB0\u5FC6",paraId:8,tocIndex:4},{value:"git",paraId:8,tocIndex:4},{value:"\u547D\u4EE4\u4E86",paraId:8,tocIndex:4},{value:"\u4EE3\u7801\u4ED3\u5E93\u5730\u5740: ",paraId:8,tocIndex:4},{value:"https://github.com/zsh-users/zsh-autosuggestions",paraId:8,tocIndex:4},{value:"\u4E00\u952E\u5B89\u88C5\uFF1A",paraId:8,tocIndex:4},{value:"git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions\n",paraId:9,tocIndex:4},{value:"zsh-syntax-highlighting",paraId:10,tocIndex:4},{value:"\u4F5C\u7528\uFF1A\u7A81\u663E\u547D\u4EE4\uFF0C\u6709\u52A9\u4E8E\u68C0\u67E5\u547D\u4EE4\uFF0C\u5F88\u6709\u5229\u4E8E\u773C\u775B\u548C\u5927\u8111",paraId:11,tocIndex:4},{value:"\u4EE3\u7801\u4ED3\u5E93\u5730\u5740\uFF1A",paraId:11,tocIndex:4},{value:"https://github.com/zsh-users/zsh-syntax-highlighting",paraId:11,tocIndex:4},{value:"\u4E00\u952E\u5B89\u88C5:",paraId:11,tocIndex:4},{value:"  git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting\n",paraId:12,tocIndex:4},{value:"git",paraId:13,tocIndex:5},{value:"\u8BE5\u63D2\u4EF6\u4E3Agit\u547D\u4EE4\u8BBE\u7F6E\u4E86\u522B\u540D\uFF0C\u4E0D\u518D\u9700\u8981\u6BCF\u6B21\u6572\u5B8C\u6574\u547D\u4EE4",paraId:14,tocIndex:5},{value:` gcb => git checkout -b
+ gco => git checkout
+ gcm => git checkout \u4E3B\u5206\u652F(master, main...)
+ gcd => git checkout \u5F00\u53D1\u5206\u652F(dev...)
+ gb => git branch
+ gba => git branch -a
+ gbd => git branch -d
+ ......
+`,paraId:15,tocIndex:5},{value:"node",paraId:16,tocIndex:5},{value:"\u8BE5\u63D2\u4EF6\u63D0\u4F9B\u5FEB\u901F\u8DF3\u8F6Cnodejs\u6587\u6863\uFF0C\u5728\u547D\u4EE4\u884C\u4E2D\u8F93\u5165",paraId:17,tocIndex:5},{value:"node-docs xxx(api)",paraId:17,tocIndex:5},{value:"\uFF0C\u5982",paraId:17,tocIndex:5},{value:"node-docs fs",paraId:17,tocIndex:5},{value:"\u5FEB\u901F\u8DF3\u8F6C\u5230",paraId:17,tocIndex:5},{value:"fsapi",paraId:17,tocIndex:5},{value:"\uFF0C\u5BF9\u4E8E\u6B63\u5728\u4F7F\u7528",paraId:17,tocIndex:5},{value:"nodejs",paraId:17,tocIndex:5},{value:"\u5F00\u53D1\u7A81\u7136\u5FD8\u8BB0\u67D0\u4E2A",paraId:17,tocIndex:5},{value:"api",paraId:17,tocIndex:5},{value:"\u9700\u8981\u67E5\u770B\u6587\u6863\uFF0C\u8FD9\u4E2A\u63D2\u4EF6\u6709\u6548\u8282\u7701\u641C\u7D22\u6587\u6863\u65F6\u95F4\u3002",paraId:17,tocIndex:5},{value:"vscode",paraId:18,tocIndex:5},{value:"\u547D\u4EE4\u884C\u8F93\u5165",paraId:19,tocIndex:5},{value:"code",paraId:19,tocIndex:5},{value:"\u6216",paraId:19,tocIndex:5},{value:"vsc",paraId:19,tocIndex:5},{value:"\u5728vscode\u4E2D\u6253\u5F00\u5F53\u524D\u8DEF\u5F84\u4E0B\u7684\u6587\u4EF6\u3002",paraId:19,tocIndex:5},{value:"web-search",paraId:20,tocIndex:5},{value:"\u5728\u9ED8\u8BA4\u6D4F\u89C8\u5668\u4E2D\u901A\u8FC7\u6307\u5B9A\u641C\u7D22\u670D\u52A1\u8FDB\u884C\u641C\u7D22\uFF0C\u641C\u7D22\u670D\u52A1\u5305\u62EC",paraId:21,tocIndex:5},{value:"google bing github...",paraId:21,tocIndex:5},{value:"\uFF0C\u4F7F\u7528\u65B9\u5F0F\u4E3A\u547D\u4EE4\u884C\u4E2D\u8F93\u5165\u641C\u7D22\u670D\u52A1 \u5173\u952E\u8BCD\uFF0C\u5982",paraId:21,tocIndex:5},{value:"google zsh",paraId:21,tocIndex:5},{value:"\u7528\u8C37\u6B4C\u5FEB\u901F\u641C\u7D22zsh\u3002",paraId:21,tocIndex:5},{value:".zsh",paraId:22},{value:"\u6267\u884C\u7F16\u8F91\u547D\u4EE4",paraId:23,tocIndex:6},{value:`nano ~/.zshrc
+`,paraId:24,tocIndex:6},{value:"\u5199\u5165plugin",paraId:25,tocIndex:6},{value:`  plugins=(
+        git
+        node
+        zsh-syntax-highlighting
+        zsh-autosuggestions
+        pnpm
+        vscode
+        web-search
+  )
+`,paraId:26,tocIndex:6},{value:"\u91CD\u7F6E",paraId:27,tocIndex:6},{value:".zsh",paraId:27,tocIndex:6},{value:"\u6587\u4EF6\u4F7F\u5176\u751F\u6548",paraId:27,tocIndex:6},{value:`source ~/.zshrc
+`,paraId:28,tocIndex:6}]},43489:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(24044);const n=[{value:`typeof \u68C0\u6D4B null \u8FD4\u56DE\u662F object
+typeof \u4E00\u4E2A\u6CA1\u6709\u503C\u7684\u53D8\u91CF\u4F1A\u8FD4\u56DE undefined`,paraId:0,tocIndex:1},{value:"null\u662F\u4E00\u4E2A\u53EA\u6709\u4E00\u4E2A\u503C\u7684\u7279\u6B8A\u7C7B\u578B\u3002\u8868\u793A\u4E00\u4E2A\u7A7A\u5BF9\u8C61\u5F15\u7528\u3002",paraId:1,tocIndex:2},{value:"undefined \u662F\u4E00\u4E2A\u6CA1\u6709\u8BBE\u7F6E\u503C\u7684\u53D8\u91CF\u3002",paraId:1,tocIndex:2},{value:"never \u662F\u5176\u5B83\u7C7B\u578B\uFF08\u5305\u62EC null \u548C undefined\uFF09\u7684\u5B50\u7C7B\u578B\uFF0C\u4EE3\u8868\u4ECE\u4E0D\u4F1A\u51FA\u73B0\u7684\u503C\u3002\u58F0\u660E\u4E3A never \u7C7B\u578B\u7684\u53D8\u91CF\u53EA\u80FD\u88AB never \u7C7B\u578B\u6240\u8D4B\u503C",paraId:1,tocIndex:2},{value:"?? \u548C || \u7684\u533A\u522B",paraId:2},{value:`\u76F8\u540C\u70B9
+`,paraId:3,tocIndex:3},{value:"\u7528\u6CD5\u76F8\u540C\uFF0C\u524D\u540E\u662F\u503C\uFF0C\u4E2D\u95F4\u7528\u7B26\u53F7\u8FDE\u63A5",paraId:4,tocIndex:3},{value:"\u503C1 ?? \u503C2",paraId:4,tocIndex:3},{value:"\u503C1 || \u503C2",paraId:4,tocIndex:3},{value:`\u4E0D\u540C\u70B9
+`,paraId:3,tocIndex:3},{value:`\u5224\u65AD\u65B9\u5F0F\u4E0D\u540C\uFF1A
+`,paraId:5,tocIndex:3},{value:"\u4F7F\u7528",paraId:6,tocIndex:3},{value:"??",paraId:6,tocIndex:3},{value:"\u65F6\uFF0C\u53EA\u6709\u5F53",paraId:6,tocIndex:3},{value:"\u503C1",paraId:6,tocIndex:3},{value:"\u4E3A",paraId:6,tocIndex:3},{value:"null",paraId:6,tocIndex:3},{value:"\u6216",paraId:6,tocIndex:3},{value:"undefined",paraId:6,tocIndex:3},{value:"\u65F6\u624D\u8FD4\u56DE",paraId:6,tocIndex:3},{value:"\u503C2",paraId:6,tocIndex:3},{value:"\u4F7F\u7528",paraId:6,tocIndex:3},{value:"||",paraId:6,tocIndex:3},{value:"\u65F6\uFF0C",paraId:6,tocIndex:3},{value:"\u503C1",paraId:6,tocIndex:3},{value:"\u4F1A\u8F6C\u6362\u4E3A\u5E03\u5C14\u503C\u5224\u65AD\uFF0C\u4E3A",paraId:6,tocIndex:3},{value:"true",paraId:6,tocIndex:3},{value:"\u65F6\u8FD4\u56DE",paraId:6,tocIndex:3},{value:"\u503C1",paraId:6,tocIndex:3},{value:",",paraId:6,tocIndex:3},{value:"false",paraId:6,tocIndex:3},{value:"\u8FD4\u56DE",paraId:6,tocIndex:3},{value:"\u503C2",paraId:6,tocIndex:3},{value:"type \u548C interface\u7684\u533A\u522B",paraId:2},{value:"\u76F8\u540C\u70B9",paraId:7,tocIndex:4},{value:"\u90FD\u53EF\u63CF\u8FF0\u4E00\u4E2A\u5BF9\u8C61\u6216\u51FD\u6570",paraId:8,tocIndex:4},{value:"interface",paraId:9,tocIndex:4},{value:`  interface User {
+    name: string
+    age: number
+  }
+  
+  interface SetUser {
+    (name: string, age: number): void;
+  }
+`,paraId:10,tocIndex:4},{value:"type",paraId:11,tocIndex:4},{value:`  type User {
+    name: string
+    age: number
+  }
+  type SetUser = (name: string, age: number)=> void
+`,paraId:12,tocIndex:4},{value:"\u90FD\u5141\u8BB8\u62D3\u5C55",paraId:13,tocIndex:4},{value:"interface\u548Ctype\u90FD\u53EF\u4EE5\u62D3\u5C55\uFF0C\u5E76\u4E14\u4E24\u8005\u5E76\u4E0D\u662F\u76F8\u4E92\u72EC\u7ACB\u7684\uFF0C\u4E5F\u5C31\u662F\u8BF4interface\u53EF\u4EE5extends type,type\u4E5F\u53EF\u4EE5 extends interface\u3002\u867D\u7136\u6548\u679C\u5DEE\u4E0D\u591A\uFF0C\u4F46\u662F\u4E24\u8005\u8BED\u6CD5\u4E0D\u540C\u3002",paraId:14,tocIndex:4},{value:"interface extends interface",paraId:15,tocIndex:4},{value:`  interface Name {
+    name: string
+  }
+  interface User extend Name  {
+    age: number
+  }
+`,paraId:16,tocIndex:4},{value:"type extends type",paraId:17,tocIndex:4},{value:`  type Name = {
+    name: string
+  }
+  type User = Name & {age: number}
+`,paraId:18,tocIndex:4},{value:"interface extend type",paraId:19,tocIndex:4},{value:`  type Name = {
+    name: string
+  }
+  interface User extends Name {
+    age: number
+  }
+`,paraId:20,tocIndex:4},{value:"type extends interface",paraId:21,tocIndex:4},{value:`  interface Name {
+    name: string
+  }
+  type User = Name &  {
+    age: number
+  }
+`,paraId:22,tocIndex:4},{value:"\u4E0D\u540C\u70B9",paraId:23,tocIndex:4},{value:"type\u53EF\u4EE5\u4F46interface\u4E0D\u53EF",paraId:24,tocIndex:4},{value:"type\u53EF\u4EE5\u58F0\u660E\u57FA\u672C\u7C7B\u578B\u522B\u540D\uFF0C\u8054\u5408\u7C7B\u578B\uFF0C\u5143\u7EC4\u7B49\u7C7B\u578B",paraId:25,tocIndex:4},{value:`  type Name = string
+
+  interface Dog {
+    wong()
+  }
+  interface Cat {
+    miao()
+  }
+
+  type Pet = Dog | Cat
+
+  type PetList = [Dog, Pet]
+`,paraId:26,tocIndex:4},{value:"type\u8BED\u53E5\u8FD8\u53EF\u4F7F\u7528typeof\u83B7\u53D6\u5B9E\u4F8B\u7684 \u7C7B\u578B\u8FDB\u884C\u8D4B\u503C",paraId:27,tocIndex:4},{value:`  let div = document.createElement('div')
+  type B = typeof div
+`,paraId:28,tocIndex:4},{value:"\u9A9A\u64CD\u4F5C",paraId:29,tocIndex:4},{value:`  type StringOrNumber = string | number;
+  type Text = string | {text: string};
+  type NameLookup = Dictionary<string, Person>;
+  type Callback<T> = (data: T) => void;
+  type Pair<T> = [T, T];
+  type Coordinates = Pair<number>;
+  type Tree<T> = T | { left: Tree<T>, right: Tree<T>};        
+`,paraId:30,tocIndex:4},{value:"interface \u53EF\u4EE5\u4F46\u662Ftype\u4E0D\u53EF\u4EE5",paraId:31,tocIndex:4},{value:"interface\u80FD\u591F\u58F0\u660E\u5408\u5E76",paraId:32,tocIndex:4},{value:`  interface User {
+    name: string
+    age: number
+  }
+
+  interface User {
+    sex: string
+  }
+
+  /*
+  User \u63A5\u53E3\u4E3A {
+    name: string
+    age: number
+    sex: string 
+  }
+  */
+`,paraId:33,tocIndex:4},{value:"\u7528\u9014\uFF1A",paraId:34,tocIndex:5},{value:"enum",paraId:34,tocIndex:5},{value:"\u7528\u4E8E\u5B9A\u4E49\u4E00\u7EC4\u5177\u540D\u7684\u5E38\u91CF\u3002\u8FD9\u662F\u4E00\u4E2A\u503C\u7684\u96C6\u5408\uFF0C\u53EF\u4EE5\u662F\u6570\u5B57\u6216\u5B57\u7B26\u4E32\u3002",paraId:34,tocIndex:5},{value:"\u7279\u70B9\uFF1A",paraId:35,tocIndex:5},{value:"\u7F16\u8BD1\u4E3AJavaScript\u4EE3\u7801",paraId:36,tocIndex:5},{value:"\u53EF\u7528\u4E8E\u63A7\u5236\u6D41\uFF0C\u4F8B\u5982\u6761\u4EF6\u548C\u8FED\u4EE3",paraId:36,tocIndex:5},{value:"\u4F8B\u5982:",paraId:37,tocIndex:5},{value:`  enum Color {
+  Red,
+  Green,
+  Blue
+}
+
+let c: Color = Color.Green;
+console.log(c); // \u8F93\u51FA 1
+`,paraId:38,tocIndex:5}]},85031:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(87942);const n=[{value:"\u5728\u4F7F\u7528",paraId:0,tocIndex:2},{value:"ant-design-vue",paraId:0,tocIndex:2},{value:`\u4E2D\uFF0C\u6D89\u53CA\u5230\u8868\u5355\u6570\u636E\u7684\u66F4\u6539\uFF0C\u671F\u671B\u5728\u63D0\u4EA4\u5B8CAPI\u8BF7\u6C42\u540E\uFF0C\u4F7F\u5F97\u8868\u5355\u7684selected rowKeys\u5168\u90E8\u6E05\u9664\u3002
+\u4F46\u662F\u4F7F\u7528`,paraId:0,tocIndex:2},{value:"reactive",paraId:0,tocIndex:2},{value:" \u6216\u8005 ",paraId:0,tocIndex:2},{value:"ref",paraId:0,tocIndex:2},{value:" \u53D1\u73B0\u5176\u8868\u5355\u7ED1\u5B9A\u7684\u53E6\u5916\u4E00\u4E2A\u54CD\u5E94\u5F0F\u5BF9\u8C61\u6539\u53D8\u65F6\uFF0C\u8868\u5355\u6570\u636E\u5E76\u4E0D\u4F1A\u540C\u6B65\u66F4\u65B0\uFF0C\u5177\u4F53\u4EE3\u7801demo\u5982\u4E0B\uFF1A",paraId:0,tocIndex:2},{value:`<script setup>
+import { reactive } from 'vue';
+import { ref, computed } from 'vue'
+
+const msg = ref('Hello World!')
+
+const extraTable = reactive({
+  rows: [],
+  keys: [1, 2, 3, '2121']
+})
+
+
+const rowSelection = reactive({
+  rowkeys: extraTable.keys,
+})
+
+const handleChange = () => {
+  // \u4E0D\u751F\u6548
+  extraTable.keys = []
+
+  // \u751F\u6548
+  // extraTable.keys.length = 0
+}
+
+<\/script>
+
+<template>
+  <h1>{{ rowSelection.rowkeys.join(',') }}</h1>
+
+  <button @click="handleChange">\u70B9\u51FB\u6539\u53D8</button>
+</template>
+
+`,paraId:1,tocIndex:2},{value:"reactive",paraId:2,tocIndex:3},{value:" \u548C ",paraId:2,tocIndex:3},{value:"ref",paraId:2,tocIndex:3},{value:" \u53EA\u4F1A\u76D1\u542C\u5BF9\u5E94\u7684",paraId:2,tocIndex:3},{value:"proxy",paraId:2,tocIndex:3},{value:"\u53D8\u5316\uFF0C\u5982\u679C\u76F4\u63A5\u8D4B\u503C",paraId:2,tocIndex:3},{value:"[]",paraId:2,tocIndex:3},{value:"\uFF0C\u90A3\u4E48\u5C06\u4F1A\u6539\u53D8\u539F\u6709\u5BF9\u8C61\u7684\u5F15\u7528\uFF0C\u4ECE\u800C\u65E0\u6CD5\u89E6\u53D1\u54CD\u5E94\u5F0F\u6539\u53D8\u3002\u800C\u4F7F\u7528",paraId:2,tocIndex:3},{value:".length=0",paraId:2,tocIndex:3},{value:"\u7684\u64CD\u4F5C\u662F\u5728\u539F\u6709\u54CD\u5E94\u5F0F\u5BF9\u8C61\u4E0A\u8FDB\u884C\u7684\u6539\u52A8\uFF0C\u4F1A\u89E6\u53D1\u5176\u503C\u53D8\u5316\u3002",paraId:2,tocIndex:3}]},76190:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(78999);const n=[{value:"Cookie \u662F\u4E00\u79CD\u7531\u670D\u52A1\u5668\u53D1\u9001\u5230\u7528\u6237\u6D4F\u89C8\u5668\u7684\u5C0F\u6570\u636E\u5757\uFF0C\u901A\u5E38\u7528\u4E8E\u5B58\u50A8\u7528\u6237\u7684\u4F1A\u8BDD\u72B6\u6001\u3001\u504F\u597D\u8BBE\u7F6E\u6216\u5176\u4ED6\u4FE1\u606F\u3002Cookie \u5728\u5BA2\u6237\u7AEF\u5B58\u50A8\uFF0C\u53EF\u4EE5\u5728\u540E\u7EED\u7684\u8BF7\u6C42\u4E2D\u53D1\u9001\u56DE\u670D\u52A1\u5668\u3002",paraId:0,tocIndex:2},{value:"\u670D\u52A1\u5668\uFF1A\u5F53\u7528\u6237\u8BBF\u95EE\u7F51\u7AD9\u65F6\uFF0C\u670D\u52A1\u5668\u53EF\u4EE5\u901A\u8FC7 HTTP \u54CD\u5E94\u5934\u4E2D\u7684 Set-Cookie \u6307\u4EE4\u53D1\u9001\u4E00\u4E2A Cookie \u7ED9\u6D4F\u89C8\u5668\u3002\u6D4F\u89C8\u5668\u968F\u540E\u4F1A\u5C06\u8FD9\u4E2A Cookie \u5B58\u50A8\u5728\u672C\u5730\u3002",paraId:1,tocIndex:3},{value:"\u5BA2\u6237\u7AEF\uFF1A\u524D\u7AEF JavaScript \u4E5F\u53EF\u4EE5\u901A\u8FC7 document.cookie \u6765\u521B\u5EFA\u6216\u4FEE\u6539 Cookie\u3002",paraId:1,tocIndex:3}]},67456:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(87725);const n=[{value:`MDN\u89E3\u91CA
+\u8DE8\u6E90\u8D44\u6E90\u5171\u4EAB\uFF08CORS\uFF0C\u6216\u901A\u4FD7\u5730\u8BD1\u4E3A\u8DE8\u57DF\u8D44\u6E90\u5171\u4EAB\uFF09\u662F\u4E00\u79CD\u57FA\u4E8E HTTP \u5934\u7684\u673A\u5236\uFF0C\u8BE5\u673A\u5236\u901A\u8FC7\u5141\u8BB8\u670D\u52A1\u5668\u6807\u793A\u9664\u4E86\u5B83\u81EA\u5DF1\u4EE5\u5916\u7684\u5176\u4ED6\u6E90\uFF08\u57DF\u3001\u534F\u8BAE\u6216\u7AEF\u53E3\uFF09\uFF0C\u4F7F\u5F97\u6D4F\u89C8\u5668\u5141\u8BB8\u8FD9\u4E9B\u6E90\u8BBF\u95EE\u52A0\u8F7D\u81EA\u5DF1\u7684\u8D44\u6E90\u3002`,paraId:0,tocIndex:1}]},34337:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(73723);const n=[{value:"This is a guide example.",paraId:0}]},37586:function(t,a,e){e.r(a),e.d(a,{texts:function(){return n}});var d=e(89489);const n=[{value:"learning-hub",paraId:0}]}}]);
